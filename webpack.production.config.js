03b2b2af6e648524c6992120fc8b94d2935a1fe0
Antoine Drouhin <antoine.drouhin@gmail.com>
2016-12-08 09:44:11 +0100

    Webpack production Config

---
+++ webpack.production.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry:
+    './source/index.js'
+  ,
+  output: {
+    filename: 'bundle.js',
+    path:  './dist',
+    publicpath: './dist'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }, {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }, {
+        test: /\.png$/,
+        loader: "url-loader?limit=100000"
+      }, {
+        test: /\.jpg$/,
+        loader: "file-loader"
+      }, {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file'
+      }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url?limit=10000&mimetype=image/svg+xml'
+      }
+    ]
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ]
+};
