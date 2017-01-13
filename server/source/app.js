f5751aafb6b673add0f0bea4f1dad2ad846d8489
Antoine Drouhin <antoine.drouhin@gmail.com>
2017-01-13 22:51:39 +0100

    Server expense delete

--- server/source/app.js
+++ server/source/app.js
@@ -48,9 +48,30 @@ app.route('/expense')
   })
   .delete((req, res) => {
     console.log('delete expense')
-    ExpenseModel.find({}).remove().exec(err => console.log(err))
+    if (req.params.length < 1){
+      ExpenseModel.find({}).remove().exec(err => console.log(err))
+    } else {
+      ExpenseModel.find(req.params[0]).remove().exec(err => console.log(err))
+    }
     res.sendStatus(200)
   })
+
+  app.route('/expense/:id')
+    .get((req, res) => {
+      console.log('get expense')
+      ExpenseModel.find(function (err, expenses) {
+        if (err) return console.error(err);
+        res.json(expenses);
+      })
+    })
+    .delete((req, res) => {
+      console.log('delete expense : id = ' + req.params.id)
+      ExpenseModel.find({_id : req.params.id}).remove().exec(err => {
+        console.log(err)
+        if (err) res.sendStatus(500)
+      })
+      res.sendStatus(200)
+    })
 
 
 module.exports = app;
