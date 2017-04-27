ee905972f46b3abb3aa34d71444f7dc9199c3c49
Antoine Drouhin <antoine.drouhin@gmail.com>
2017-04-27 18:08:42 +0200

    https documentation

--- server/source/server.js
+++ server/source/server.js
@@ -2,31 +2,18 @@ require('dotenv').config()
 
 const app = require('app')
 const https = require('https')
-const http = require('http')
-const fs = require("fs");
+const fs = require('fs')
 
 const HTTP_PORT = process.env.HTTP_PORT
 const HTTPS_PORT = process.env.HTTPS_PORT
 
 if (!HTTP_PORT) {
-  throw error("HTTP port is undefined")
+  throw 'HTTP port is undefined'
 }
 
 if (!HTTPS_PORT) {
-  throw error("HTTPS port is undefined")
+  throw 'HTTPS port is undefined'
 }
-
-
-/*
-Notes Aurélien :
-go to the server location path.
-cd to keys
-then :
-:openssl genrsa 2048 > private.key
-:openssl req -new -key private.key -out cert.csr
-:openssl x509 -req -in cert.csr -signkey private.key -out certificate.pem
-
-*/
 
 // Declare key and certificate
 const keyCert = {
@@ -35,10 +22,11 @@ const keyCert = {
 }
 
 // Mount HTTPS server
-var httpsServer = https.createServer(keyCert, app)
+/*var httpsServer =*/ https.createServer(keyCert, app)
   .listen(HTTPS_PORT, function () {
-    console.log('HTTPS Server listening on port ' + HTTPS_PORT);
-});
+    console.log('HTTPS Server listening on port ' + HTTPS_PORT)
+  })
+
 // Mount HTTP server (disabled)
 // var httpServer = http.createServer(app).listen(HTTP_PORT, function() {
 //   console.log('HTTP Server listening on port ' + HTTP_PORT);
