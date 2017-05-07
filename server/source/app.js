225841df4f35dbbe11a7de64116d9c2b36abb2e2
Aurélien Garret <aurelien.garret@gmail.com>
2017-05-07 10:09:41 +0200

    Import remember-me example

--- server/source/app.js
+++ server/source/app.js
@@ -24,7 +24,9 @@ app.use(bodyParser.json())
 // Configuring Passport
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
-require('./config/passport.js')(passport, LocalStrategy)
+const RememberMeStrategy = require('passport-remember-me').Strategy
+
+//require('./config/passport.js')(passport, LocalStrategy)
 const expressSession = require('express-session')
 app.use(expressSession({
   secret: 'mySecretKey',
@@ -32,8 +34,107 @@ app.use(expressSession({
     secure: true
   }
 }))
+
+
+
+/* Fake, in-memory database of users */
+
+var users = [
+    { id: 1, username: 'bob', password: 'secret', email: 'bob@example.com' }
+  , { id: 2, username: 'joe', password: 'birthday', email: 'joe@example.com' }
+];
+
+function findById(id, fn) {
+  var idx = id - 1;
+  if (users[idx]) {
+    fn(null, users[idx]);
+  } else {
+    fn(new Error('User ' + id + ' does not exist'));
+  }
+}
+
+function findByUsername(username, fn) {
+  for (var i = 0, len = users.length; i < len; i++) {
+    var user = users[i];
+    if (user.username === username) {
+      return fn(null, user);
+    }
+  }
+  return fn(null, null);
+}
+
+/* Fake, in-memory database of remember me tokens */
+
+var tokens = {}
+
+function consumeRememberMeToken(token, fn) {
+  var uid = tokens[token];
+  // invalidate the single-use token
+  delete tokens[token];
+  return fn(null, uid);
+}
+
+function saveRememberMeToken(token, uid, fn) {
+  tokens[token] = uid;
+  return fn();
+}
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // Find the user by username.  If there is no user with the given
+      // username, or the password is not correct, set the user to `false` to
+      // indicate failure and set a flash message.  Otherwise, return the
+      // authenticated `user`.
+      findByUsername(username, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false, { message: 'Unknown user ' + username }); }
+        if (user.password != password) { return done(null, false, { message: 'Invalid password' }); }
+        return done(null, user);
+      })
+    });
+  }
+));
+
+passport.use(new RememberMeStrategy(
+  function(token, done) {
+    consumeRememberMeToken(token, function(err, uid) {
+      if (err) { return done(err); }
+      if (!uid) { return done(null, false); }
+
+      findById(uid, function(err, user) {
+        if (err) { return done(err); }
+        if (!user) { return done(null, false); }
+        return done(null, user);
+      });
+    });
+  },
+  issueToken
+));
+
+function issueToken(user, done) {
+  var token = utils.randomString(64);
+  saveRememberMeToken(token, user.id, function(err) {
+    if (err) { return done(err); }
+    return done(null, token);
+  });
+}
+
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(passport.authenticate('remember-me'));
 
 // redirect HTTP to HTTPS
 // app.all('*', function(req, res, next){
@@ -52,8 +153,24 @@ app.get('/', (req, res) => {
 const expense = require('./route/expense.js')
 app.use('/expense', expense)
 
-const login = require('./route/login.js')
-app.use('/login', login)
+//const login = require('./route/login.js')
+//app.use('/login', login)
+
+app.post('/login',
+  passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+  function(req, res, next) {
+    // Issue a remember me cookie if the option was checked
+    if (!req.body.remember_me) { return next(); }
+
+    issueToken(req.user, function(err, token) {
+      if (err) { return next(err); }
+      res.cookie('remember_me', token, { path: '/', httpOnly: true, maxAge: 604800000 });
+      return next();
+    });
+  },
+  function(req, res) {
+    res.json({"connecter":true});
+  });
 
 const user = require('./route/user.js')
 app.use('/user', user)
