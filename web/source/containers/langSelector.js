9f864f26fcb0f6bc022a1b7f73be28688712cbb4
Aurélien Garret <aurelien.garret@gmail.com>
2017-06-07 16:23:01 +0200

    Optimise lang selector component

--- web/source/containers/langSelector.js
+++ web/source/containers/langSelector.js
@@ -3,19 +3,36 @@ import React, { PropTypes }   from 'react'
 import { connect } from 'react-redux'
 import { DropdownButton, MenuItem } from 'react-bootstrap'
 import { setLang } from '../actions/lang_actions'
-import translate from '../lang/language.js'
 import flag_fr from '../img/flags/France.png'
 import flag_en from '../img/flags/UnitedKingdom.png'
+import translate from '../lang/language.js'
 
 const LangSelector = (props) => {
   let style = {
-    width: '30px',
-    height : '30px'
+    width: '20px',
+    height : '20px'
   }
+
+  function getImageFlag() {
+    let current_flag
+    switch (props.lang) {
+    case 'fr':
+      current_flag=flag_fr
+      break
+    case 'en':
+      current_flag=flag_en
+      break
+    }
+    return <img style={style} src={current_flag}/>
+  }
+
+
+
+
   return (
-    <DropdownButton title={translate(props.lang, 'LANG_SELECTOR')} id="bg-nested-dropdown">
-      <MenuItem onClick={()=>props.onChange('fr')} eventKey="fr"><img style={style} src={flag_fr}/> {translate(props.lang, 'FR')}</MenuItem>
-      <MenuItem onClick={()=>props.onChange('en')} eventKey="en"><img style={style} src={flag_en}/> {translate(props.lang, 'EN')}</MenuItem>
+    <DropdownButton title={getImageFlag()} id="bg-nested-dropdown" pullRight={true}>
+      <MenuItem onClick={()=>props.onChange('fr')} eventKey="fr"><img style={style} src={flag_fr}/> {translate(props.lang, "FR")}</MenuItem>
+      <MenuItem onClick={()=>props.onChange('en')} eventKey="en"><img style={style} src={flag_en}/> {translate(props.lang, "EN")}</MenuItem>
     </DropdownButton>
   )
 }
