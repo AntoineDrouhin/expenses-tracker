500412fca9af0e9ef6497ac2d3fa74d5b853a629
Antoine Drouhin <antoine.drouhin@gmail.com>
2017-06-07 10:03:22 +0200

    refactor : expensesTypes => expenseTypes

--- web/source/reducers/index.js
+++ web/source/reducers/index.js
@@ -1,7 +1,7 @@
 
 import { combineReducers } from 'redux'
 import expenses from './expenses'
-import expensesTypes from './expensesTypes'
+import expenseTypes from './expenseTypes'
 import user from './user'
 
 import displayOption from './displayOption'
@@ -17,7 +17,7 @@ const rootReducer = function(state, action) {
 
 const appReducer = combineReducers({
   expenses,
-  expensesTypes,
+  expenseTypes,
   user,
   displayOption,
   global_state
